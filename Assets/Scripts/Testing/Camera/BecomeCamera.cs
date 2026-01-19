37a8b996fa37a58c2417b4696a7bea08bded20bc
EnokNord <51075222+EnokNord@users.noreply.github.com>
2026-01-19 19:18:55 +0100

    Added comments

--- Assets/Scripts/Testing/Camera/BecomeCamera.cs
+++ Assets/Scripts/Testing/Camera/BecomeCamera.cs
@@ -4,44 +4,41 @@ using UnityEngine;
 
 public class BecomeCamera : MonoBehaviour
 {
+    // Checks if it is the main camrea or not
     private bool currentCamera = false;
     private bool secondHit = false; //due to the players hit boxes the game regirsters players hiting this twice. Need a fix for final version
 
+    // The Layer the payer is on. Used for a hit check
     private const int PlayerLayer = 7;
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
-    {
-        
-    }
 
-    // Update is called once per frame
-    void Update()
-    {
 
-    }
-
+    //The cameras use 2D coldiers that cover their enteir space to check if the player should switch to the camrea or not.
     private void OnTriggerEnter2D(Collider2D other)
     {
-        
+
         if (other.gameObject.layer != PlayerLayer) return;
 
         if (!currentCamera)
         {
-            //Debug.Log("Work damit");
+            //Blocks a dubble hit bug
             if (!secondHit)
             {
                 secondHit = true;
                 return;
             }
+            //Sets the camera as the main camera
             gameObject.GetComponent<CinemachineCamera>().Priority = 1;
             currentCamera = true;
             secondHit = false;
         }
     }
+
+    //When the player leaves the cameras zone, it will relinquish the camrea roll to the players camera
     private void OnTriggerExit2D(Collider2D other)
     {
         if (other.gameObject.layer != PlayerLayer) return;
 
+        //Removes the camrea from the main camrea
         gameObject.GetComponent<CinemachineCamera>().Priority = -1;
         currentCamera = false;
         secondHit = false;
