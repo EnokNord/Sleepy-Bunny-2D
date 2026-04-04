7c31f3d461ea876cc0a6a77f1cba5ebbe6cc9e14
Mina <Wilhelmina.eleonora@gmail.com>
2026-04-04 16:33:55 +0200

    Added drowning mechanic

--- Assets/Scripts/Mina's Scripts/Entities/PlayerHealthComponent.cs
+++ Assets/Scripts/Mina's Scripts/Entities/PlayerHealthComponent.cs
@@ -5,11 +5,18 @@ public class PlayerHealthComponent : HealthComponent
 {
     [SerializeField] float deathHeight = 30;
     [SerializeField] float impactVelocityThreshold = 40;
+    [SerializeField] float swimTimeTilDeath = 10;
+
+
+
     PlayerInputManager inputManager;
     Rigidbody2D rb;
     bool falling;
     float fallHeight;
     float impactVelocity = 0;
+    float swimTimer = 0;
+    bool swimming = false;
+    bool inWater = false;
     protected override void Initialize()
     {
         inputManager = GetComponent<PlayerInputManager>();
@@ -29,7 +36,20 @@ public class PlayerHealthComponent : HealthComponent
             falling = false;
             impactVelocity = 0;
         }
-        
+        if (swimming)
+        {
+            swimTimer -= Time.fixedDeltaTime;
+            if(swimTimer <= 0)
+            {
+                TriggerDeathEvent();
+            }
+            
+        }
+        if (!inWater && swimTimer != 0 && Global.GlobalFunctionsLibrary.IsGrounded(rb))
+        {
+             swimming = false;
+             swimTimer = 0;
+        }
     }
     private void Awake()
     {
@@ -50,5 +70,20 @@ public class PlayerHealthComponent : HealthComponent
     private void OnCollisionEnter2D(Collision2D collision)
     {
         impactVelocity = collision.relativeVelocity.y;
+    }
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        swimming = false;
+        inWater = false;
+    }
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        BuoyancyEffector2D buoyancy = collision.GetComponent<BuoyancyEffector2D>();
+        if (buoyancy)
+        {
+            if(swimTimer == 0) swimTimer = swimTimeTilDeath;
+            swimming = true;
+            inWater = true;
+        }
     }
 }
